c896220638d91609aaa08d81b64b4112bac56a29
DYSTAR14 <ydeepak141097@gmail.com>
2021-07-01 18:13:22 +0530

    Dictionary To Array

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -x
 read -p "Enter 3 number : " a b c
 echo $a $b $c
+declare -A compute
 compute[1]=$((a+b*c))
 compute[2]=$((a*b+c))
 compute[3]=$((c+a/b))
 compute[4]=$((a%b+c))
 echo ${compute[@]}
-
+count=1
+for ((i=0;i<4;i++))
+do
+	array[$i]=${compute[$((count++))]}
+done
+echo ${array[@]}
