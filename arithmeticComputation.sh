49ca62f2ae679c226d18c8215e1f1df9c2bf4008
DYSTAR14 <ydeepak141097@gmail.com>
2021-07-01 18:53:07 +0530

    Result in Descending order

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,15 +1,27 @@
 #!/bin/bash -x
 read -p "Enter 3 number : " a b c
-echo $a $b $c
 declare -A compute
 compute[1]=$((a+b*c))
 compute[2]=$((a*b+c))
 compute[3]=$((c+a/b))
 compute[4]=$((a%b+c))
-echo ${compute[@]}
 count=1
 for ((i=0;i<4;i++))
 do
 	array[$i]=${compute[$((count++))]}
 done
 echo ${array[@]}
+for ((j=0;j<4-1;j++))
+do
+	for ((k=j+1;k<4;k++))
+	do
+		if [ ${array[k]} -gt ${array[j]} ];then
+			temp=${array[k]}
+			array[$k]=${array[j]}
+			array[$j]=$temp
+		fi
+	done
+done
+echo ${array[@]}
+
+
